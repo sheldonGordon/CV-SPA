4fc445ffe8e2095c4e84acac380c6ae8e64eb2a7
sheldonGordon <47449935+sheldonGordon@users.noreply.github.com>
2022-06-30 13:05:51 +0200

    ajout du panneau console

--- src/script.js
+++ src/script.js
@@ -1,26 +1,34 @@
 let isLeftDragging = false;
+let isBottomDragging = false;
 let dragbarWidth = 6;
 let page = document.getElementById("page");
 
 window.ResetColumnSizes = function ResetColumnSizes() {
   page.style.gridTemplateColumns = "auto 6px auto auto";
-}
+};
 
-window.SetCursor = function SetCursor(cursor) {
+function SetCursor(cursor) {
   page.style.cursor = cursor;
 }
 
-window.StartDrag = function StartDrag() {
+window.StartLeftDrag = function StartLeftDrag() {
   isLeftDragging = true;
 
   SetCursor("ew-resize");
-}
+};
+
+window.StartBottomDrag = function StartBottomDrag() {
+  isBottomDragging = true;
+
+  SetCursor("ns-resize");
+};
 
 window.EndDrag = function EndDrag() {
   isLeftDragging = false;
+  isBottomDragging = false;
 
   SetCursor("auto");
-}
+};
 
 window.OnDrag = function OnDrag(event) {
   if (isLeftDragging) {
@@ -31,8 +39,31 @@ window.OnDrag = function OnDrag(event) {
       "auto"
     ];
 
-    page.style.gridTemplateColumns = cols.map((c) => c.toString() === "auto" ? c.toString() : c.toString() + "px").join(" ");
+    page.style.gridTemplateColumns = cols
+      .map((c) =>
+        c.toString() === "auto" ? c.toString() : c.toString() + "px"
+      )
+      .join(" ");
 
     event.preventDefault();
   }
-}
+
+  if (isBottomDragging) {
+    let rows = [
+      "auto",
+      "auto",
+      "auto",
+      "auto",
+      dragbarWidth,
+      page.clientHeight - 2 * dragbarWidth - event.clientY
+    ];
+
+    page.style.gridTemplateRows = rows
+      .map((c) =>
+        c.toString() === "auto" ? c.toString() : c.toString() + "px"
+      )
+      .join(" ");
+
+    event.preventDefault();
+  }
+};
