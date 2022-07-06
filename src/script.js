e6eb19cdd252da18f7d4403e2f7585e09dc3716b
aurelien <achatelain62000@gmail.com>
2022-07-06 17:18:34 +0200

    Gestion des couleurs sur la partie explorer

--- src/script.js
+++ src/script.js
@@ -71,11 +71,11 @@ window.OnDrag = function OnDrag(event) {
 };
 
 //############## TREE ##############
-let toggler = document.getElementsByClassName("caret");
+let toggler = document.getElementsByClassName("arrow");
 
 for (let i = 0; i < toggler.length; i++) {
   toggler[i].addEventListener("click", function () {
     this.parentElement.querySelector(".nested").classList.toggle("active");
-    this.classList.toggle("caret-down");
+    this.classList.toggle("arrow-down");
   });
 }
