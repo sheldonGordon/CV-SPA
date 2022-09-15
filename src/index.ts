63d94c66e8df0300134977fee589e95a4d8d71a8
aurelien <achatelain62000@gmail.com>
2022-09-15 21:27:47 +0200

    mise en place de locomotive js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 // here you put all the js you want.
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
