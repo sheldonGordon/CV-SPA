e1ad29fca59109dd4ecee785aa6ddca69c2f4dfc
aurelien <achatelain62000@gmail.com>
2022-10-11 20:03:15 +0200

    Clean

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
 // here you put all the js you want.
-import LocomotiveScroll from 'locomotive-scroll';
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
